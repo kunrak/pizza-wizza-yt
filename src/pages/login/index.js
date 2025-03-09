2cc22a411f231de7eb976afbee34af5e787dcbef
kunrak <rakeshkunducsgi@gmail.com>
2025-03-10 01:13:16 +0530

    Login Page

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,7 +1,90 @@
-import React from "react";
+import Link from "next/link";
+import React, { useState } from "react";
 
 function Login() {
-  return <div>Login</div>;
+  const [credentials, setCredentials] = useState({
+    username: "",
+    password: "",
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const handleChange = (e) => {
+    setCredentials({
+      ...credentials,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  return (
+    <div
+      style={{
+        height: "90vh",
+        backgroundImage:
+          'url("https://images.pexels.com/photos/326278/pexels-photo-326278.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")',
+        backgroundSize: "cover",
+      }}
+      className="flex justify-center items-center"
+    >
+      <div className="container w-full max-w-md">
+        <form
+          onSubmit={handleSubmit}
+          action=""
+          className="bg-gray-100 dark:bg-gray-900 dark:text-gray-100 border-gradient rounded-lg shodow-2xl px-8 pt-6 pb-8 mb-4"
+        >
+          <div className="mb-4">
+            <label
+              htmlFor="email"
+              className="block text-gray-300 text-sm font-bold mb-2"
+            >
+              Username
+            </label>
+            <input
+              name="email"
+              type="text"
+              placeholder="Enter your email/username"
+              className="shadow appearance-none border border-gray-300 rounded w-full py-2 px-3 focus:border-indigo-700 text-gray-700 dark:text-gray-100  leading-tight focus:outline-none focus:shadow-outline"
+              value={credentials.username}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              htmlFor="password"
+              className="block text-gray-300 text-sm font-bold mb-2"
+            >
+              Password
+            </label>
+            <input
+              name="password"
+              type="password"
+              placeholder="********"
+              className="shadow appearance-none border border-gray-300 rounded w-full py-2 px-3 focus:border-indigo-700 text-gray-700 dark:text-gray-100  leading-tight focus:outline-none focus:shadow-outline"
+              value={credentials.password}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="flex items-center justify-between"></div>
+          <button
+            type="submit"
+            className="border font-bold dark:border-gray-400 border-gray-900 rounded mr-2 p-2 hover:bg-gradient-to-r from-indigo-700 via-violet-700 to-orange-700 hover:text-gray-100"
+          >
+            Log in
+          </button>
+          <Link href="/signup" style={{ all: "unset" }}>
+            <button
+              type="button"
+              className="border font-bold dark:border-gray-400 border-gray-900 rounded mr-2 p-2 hover:bg-gradient-to-r from-indigo-700 via-violet-700 to-orange-700 hover:text-gray-100"
+            >
+              Sign up
+            </button>
+          </Link>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
