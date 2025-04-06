31c003a8ef13506077ef296c873dcb6d992a336b
kunrak <rakeshkunducsgi@gmail.com>
2025-04-07 02:25:55 +0530

    Creating 1st API

---
+++ src/pages/api/foodData.js
@@ -0,0 +1,20 @@
+import PizzaData from "@/models/pizzaData";
+import db from "@/utils/db";
+
+export default async function handler(req, res) {
+  if (req.method !== "GET") {
+    await db.connect();
+    for (let i = 0; i < req.body.length; i++) {
+        let pizza = new PizzaData({
+            name: req.body[i].name,
+            category: req.body[i].category,
+            foodType: req.body[i].foodType,
+            price: req.body[i].price,
+            description: req.body[i].description,
+            img: req.body[i].img,
+        });
+        await pizza.save();
+    }
+    res.status(200).json({ name: "Doneeee" });
+  }
+}
