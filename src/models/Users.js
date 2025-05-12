bf508aa89fe0de6a57fa11bddba9ede19b695471
kunrak <rakeshkunducsgi@gmail.com>
2025-05-13 01:52:34 +0530

    User Model

---
+++ src/models/Users.js
@@ -0,0 +1,19 @@
+const { default: mongoose } = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+    {
+        name: { type: String, required: true },
+        email: { type: String, required: true, unique: true },
+        password: { type: String, required: true },
+        price: { type: Object, required: true },
+        location: { type: String, required: true },
+        isAdmin: { type: Boolean, default: false },
+        date: { type: Date, default: Date.now },
+      },
+
+    { timestamps: true }
+)
+
+const Users = mongoose.models.Users || mongoose.model("Users", userSchema);
+
+export default Users;
